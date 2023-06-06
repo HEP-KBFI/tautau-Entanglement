0f867259ef19a721c5d9eac300c62adba2e1e6b8
Christian Veelken <christian.veelken@cern.ch>
2023-06-06 12:45:33 +0300

    switched to latest Ntuples added replacement templates for sed

--- test/analyzeEntanglementNtuple_cfg.py
+++ test/analyzeEntanglementNtuple_cfg.py
@@ -17,8 +17,8 @@ process.fwliteOutput = cms.PSet(
 process.analyzeEntanglementNtuple = cms.PSet(
     treeName = cms.string('ntupleProducer/piPlus_piMinus'),
 
-    ##minVisTauPt = cms.double(20.),
-    ##maxAbsVisTauEta = cms.double(2.3),
+    #minVisTauPt = cms.double(20.),
+    #maxAbsVisTauEta = cms.double(2.3),
     minVisTauPt = cms.double(0.),
     maxAbsVisTauEta = cms.double(1.e+3),
     maxNumChargedKaons = cms.int32(0),
@@ -31,17 +31,35 @@ process.analyzeEntanglementNtuple = cms.PSet(
     isDEBUG = cms.bool(False)
 )
 
-inputFilePath = '/scratch/persistent/veelken/Entanglement/ntuples/2023Jun01/'
+inputFilePath = '/scratch/persistent/veelken/Entanglement/ntuples/2023Jun02/'
 inputFileNames = None
-processName = "ggH_htt"
+processName = "ggH_htt_pythia8"
 hAxis = "higgs"
 outputFileName = 'analyzeEntanglementNtuple_%s_%s.root' % (processName, hAxis)
+
+treeName = 'ntupleProducer/piPlus_piMinus'
+#minVisTauPt = 20.
+#maxAbsVisTauEta = 2.3
+minVisTauPt = 0.
+maxAbsVisTauEta = 1.e+3
+maxNumChargedKaons = 0
+maxNumNeutralKaons = 0
+maxNumPhotons = -1
+maxSumPhotonEn = 5.
 
 ##inputFilePath = None
 ##inputFileNames = $inputFileNames
 ##processName = "$processName"
 ##hAxis = "$hAxis"
 ##outputFileName = "$outputFileName"
+
+##treeName = "$treeName"
+##minVisTauPt = $minVisTauPt
+##maxAbsVisTauEta = $maxAbsVisTauEta
+##maxNumChargedKaons = $maxNumChargedKaons
+##maxNumNeutralKaons = $maxNumNeutralKaons
+##maxNumPhotons = $maxNumPhotons
+##maxSumPhotonEn = $maxSumPhotonEn
 
 inputFile_regex = r"entanglementNtuple_%s_%sAxis_[0-9]+.root" % (processName, hAxis)
 
@@ -57,5 +75,13 @@ else:
 #--------------------------------------------------------------------------------
 
 process.fwliteInput.fileNames = cms.vstring(inputFileNames)
+
 process.fwliteOutput.fileName = cms.string(outputFileName)
 
+process.analyzeEntanglementNtuple.treeName = treeName
+process.analyzeEntanglementNtuple.minVisTauPt = minVisTauPt
+process.analyzeEntanglementNtuple.maxAbsVisTauEta = maxAbsVisTauEta
+process.analyzeEntanglementNtuple.maxNumChargedKaons = maxNumChargedKaons
+process.analyzeEntanglementNtuple.maxNumNeutralKaons = maxNumNeutralKaons
+process.analyzeEntanglementNtuple.maxNumPhotons = maxNumPhotons
+process.analyzeEntanglementNtuple.maxSumPhotonEn = maxSumPhotonEn
--- test/makeControlPlots_cfg.py
+++ test/makeControlPlots_cfg.py
@@ -16,8 +16,8 @@ process.fwliteOutput = cms.PSet(
 process.makeControlPlots = cms.PSet(
     treeName = cms.string('ntupleProducer/piPlus_piMinus'),
 
-    ##minVisTauPt = cms.double(20.),
-    ##maxAbsVisTauEta = cms.double(2.3),
+    #minVisTauPt = cms.double(20.),
+    #maxAbsVisTauEta = cms.double(2.3),
     minVisTauPt = cms.double(0.),
     maxAbsVisTauEta = cms.double(1.e+3),
     maxNumChargedKaons = cms.int32(0),
@@ -36,11 +36,29 @@ processName = "ggH_htt"
 hAxis = "higgs"
 outputFileName = 'makeControlPlots_%s_%s.root' % (processName, hAxis)
 
+treeName = 'ntupleProducer/piPlus_piMinus'
+#minVisTauPt = 20.
+#maxAbsVisTauEta = 2.3
+minVisTauPt = 0.
+maxAbsVisTauEta = 1.e+3
+maxNumChargedKaons = 0
+maxNumNeutralKaons = 0
+maxNumPhotons = -1
+maxSumPhotonEn = 5.
+
 ##inputFilePath = None
 ##inputFileNames = $inputFileNames
 ##processName = "$processName"
 ##hAxis = "$hAxis"
 ##outputFileName = "$outputFileName"
+
+##treeName = "$treeName"
+##minVisTauPt = $minVisTauPt
+##maxAbsVisTauEta = $maxAbsVisTauEta
+##maxNumChargedKaons = $maxNumChargedKaons
+##maxNumNeutralKaons = $maxNumNeutralKaons
+##maxNumPhotons = $maxNumPhotons
+##maxSumPhotonEn = $maxSumPhotonEn
 
 inputFile_regex = r"entanglementNtuple_%s_%sAxis_[0-9]+.root" % (processName, hAxis)
 
@@ -56,5 +74,14 @@ else:
 #--------------------------------------------------------------------------------
 
 process.fwliteInput.fileNames = cms.vstring(inputFileNames)
+
 process.fwliteOutput.fileName = cms.string(outputFileName)
+
+process.makeControlPlots.treeName = treeName
+process.makeControlPlots.minVisTauPt = minVisTauPt
+process.makeControlPlots.maxAbsVisTauEta = maxAbsVisTauEta
+process.makeControlPlots.maxNumChargedKaons = maxNumChargedKaons
+process.makeControlPlots.maxNumNeutralKaons = maxNumNeutralKaons
+process.makeControlPlots.maxNumPhotons = maxNumPhotons
+process.makeControlPlots.maxSumPhotonEn = maxSumPhotonEn
 
