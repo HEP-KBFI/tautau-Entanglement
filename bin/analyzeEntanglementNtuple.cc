89e4ccc130a27cc0f6be7b9315610dd6acadc22c
Christian Veelken <christian.veelken@cern.ch>
2023-08-17 15:22:57 +0300

    removed code related to maximum-likelihood fit

--- bin/analyzeEntanglementNtuple.cc
+++ bin/analyzeEntanglementNtuple.cc
@@ -15,14 +15,8 @@
 #include "TauAnalysis/Entanglement/interface/printEigenVectors_and_EigenValues.h" // printEigenVectors_and_EigenValues()
 
 #include "Math/Functions.h"                                                       // ROOT::Math::Transpose() 
-#include <Math/Functor.h>                                                         // ROOT::Math::Functor
-#include <Minuit2/Minuit2Minimizer.h>                                             // ROOT::Minuit2::Minuit2Minimizer
 #include <TBenchmark.h>                                                           // TBenchmark
-#include <TCanvas.h>                                                              // TCanvas
 #include <TError.h>                                                               // gErrorAbortLevel, kError
-#include <TGraph.h>                                                               // TGraph
-#include <TH1.h>                                                                  // TH1F
-#include <TLegend.h>                                                              // TLegend
 #include <TMath.h>                                                                // TMath::Nint()
 #include <TObject.h>                                                              // TObject
 #include <TRandom3.h>                                                             // TRandom3
@@ -181,8 +175,6 @@ class EntanglementDataset : public TObject
    std::vector<double> par_gen_;
 };
 
-static const EntanglementDataset* gEntanglementDataset = nullptr;
-
 EntanglementDataset
 build_bootstrap_sample(const EntanglementDataset& dataset, TRandom& rnd, int maxEvents_afterCuts = -1)
 {
@@ -195,80 +187,6 @@ build_bootstrap_sample(const EntanglementDataset& dataset, TRandom& rnd, int max
     bootstrap_sample.push_back(entry);
   }
   return bootstrap_sample;
-}
-
-double
-get_p(const double* par, const EntanglementData& entry)
-{
-  double Bp_r = par[0];
-  double Bp_n = par[1];
-  double Bp_k = par[2];
-
-  double Bm_r = par[3];
-  double Bm_n = par[4];
-  double Bm_k = par[5];
-
-  double C_rr = par[6];
-  double C_rn = par[7];
-  double C_rk = par[8];
-  double C_nr = par[9];
-  double C_nn = par[10];
-  double C_nk = par[11];
-  double C_kr = par[12];
-  double C_kn = par[13];
-  double C_kk = par[14];
-
-  // CV: Equation for probability p of tau pair to be in given spin state
-  //     taken from Eq. (2.6) of the paper Comput.Phys.Commun. 64 (1990) 275.
-  //     The signs for the terms Bp_X and C_XX, in which the tau+ enters, are flipped, 
-  //     because helicity frame defined with respect to tau- (and not tau+) direction in EntanglementNtupleProducer.cc code.
-  double p = 1. 
-    + (Bp_r*entry.get_hPlus_r() + Bp_n*entry.get_hPlus_n()  + Bp_k*entry.get_hPlus_k())
-    - (Bm_r*entry.get_hMinus_r() + Bm_n*entry.get_hMinus_n() + Bm_k*entry.get_hMinus_k())
-    - (C_rr*entry.get_hPlus_r()*entry.get_hMinus_r() + C_rn*entry.get_hPlus_r()*entry.get_hMinus_n() + C_rk*entry.get_hPlus_r()*entry.get_hMinus_k())
-    - (C_nr*entry.get_hPlus_n()*entry.get_hMinus_r() + C_nn*entry.get_hPlus_n()*entry.get_hMinus_n() + C_nk*entry.get_hPlus_n()*entry.get_hMinus_k())
-    - (C_kr*entry.get_hPlus_k()*entry.get_hMinus_r() + C_kn*entry.get_hPlus_k()*entry.get_hMinus_n() + C_kk*entry.get_hPlus_k()*entry.get_hMinus_k());
-  const double epsilon = 1.e-12;
-  if ( p < epsilon ) p = epsilon;
-
-  return p;
-}
-
-double
-mlfit_fcn(const double* par)
-{
-  const EntanglementDataset* mlfitData = gEntanglementDataset;
-  assert(mlfitData);
-
-  size_t numEntries = mlfitData->size();
-
-  double par_gen[npar];
-  for ( size_t idxPar = 0; idxPar < npar; ++idxPar )
-  {
-    par_gen[idxPar] = mlfitData->get_par_gen(idxPar);
-  }
-
-  double norm = 0.;
-  for ( size_t idxEntry = 0; idxEntry < numEntries; ++idxEntry )
-  {
-    const EntanglementData& entry = mlfitData->at(idxEntry);
-
-    norm += entry.get_evtWeight()*get_p(par, entry)/get_p(par_gen, entry);
-    //norm += get_p(par, entry)/get_p(par_gen, entry);
-  }
-
-  double logL = 0.;
-  for ( size_t idxEntry = 0; idxEntry < numEntries; ++idxEntry )
-  {
-    const EntanglementData& entry = mlfitData->at(idxEntry);
-
-    double p = get_p(par, entry);
-
-    logL -= 2.*entry.get_evtWeight()*log(p/norm);
-    //logL -= 2.*log(p/norm);
-  }
-
-  return logL;
 }
 
 double
@@ -390,8 +308,8 @@ class Measurement
 };
 
 Measurement
-comp_Bp_Bm_C_by_summation(const EntanglementDataset& dataset, 
-                          int verbosity = -1)
+comp_Bp_Bm_C(const EntanglementDataset& dataset, 
+             int verbosity = -1)
 {
   math::Vector3 Bp;
   math::Vector3 BpErr;
@@ -445,290 +363,6 @@ comp_Bp_Bm_C_by_summation(const EntanglementDataset& dataset,
   Bm *= (1./evtWeight_sum);
 
   C *= (1./evtWeight_sum);
-
-  Measurement measurement(Bp, BpErr, Bm, BmErr, C, CErr, verbosity);
-  return measurement;
-}
-
-std::map<size_t, std::string>
-get_parNames()
-{
-  // define parameters for maximum-likelihood (ML) fit
-  std::map<size_t, std::string> parNames;
-  parNames[0]  = "Bp_r";
-  parNames[1]  = "Bp_n";
-  parNames[2]  = "Bp_k";
-  parNames[3]  = "Bm_r";
-  parNames[4]  = "Bm_n";
-  parNames[5]  = "Bm_k";
-  parNames[6]  = "C_rr";
-  parNames[7]  = "C_rn";
-  parNames[8]  = "C_rk";
-  parNames[9]  = "C_nr";
-  parNames[10] = "C_nn";
-  parNames[11] = "C_nk";
-  parNames[12] = "C_kr";
-  parNames[13] = "C_kn";
-  parNames[14] = "C_kk";
-  return parNames;
-}
-
-TGraph*
-scan_mlfit_fixed(const std::vector<double>& parValues,
-                 size_t parToScan, const std::string& parName, size_t numPoints, double xMin, double xMax,
-                 double yMin)
-{
-  std::cout << "<scan_mlfit_fixed>:\n";
-
-  assert(xMax > xMin && numPoints >= 2);
-  double dx = (xMax - xMin)/(numPoints - 1);
-
-  double par[npar];
-  for ( size_t idxPar = 0; idxPar < npar; ++idxPar )
-  {
-    par[idxPar] = parValues[idxPar];
-  }
-
-  TGraph* graph = new TGraph(numPoints);
-  for ( size_t idxPoint = 0; idxPoint < numPoints; ++idxPoint )
-  {
-    double x = xMin + idxPoint*dx;
-    par[parToScan] = x;
-    double y = mlfit_fcn(par);
-    std::cout << " point #" << idxPoint << ": x = " << x << ", y = " << y - yMin << "\n";
-    graph->SetPoint(idxPoint, x, y - yMin);
-  }
-  return graph;
-}
-
-TGraph*
-scan_mlfit_profiled(ROOT::Math::Minimizer* mlfit,
-                    const std::vector<double>& parValues,
-                    size_t parToScan, const std::string& parName, size_t numPoints, double xMin, double xMax,
-                    double yMin)
-{
-  std::cout << "<scan_mlfit_profiled>:\n";
-
-  assert(xMax > xMin && numPoints >= 2);
-  double dx = (xMax - xMin)/(numPoints - 1);
-
-  for ( size_t idxPar = 0; idxPar < npar; ++idxPar )
-  {
-    mlfit->SetVariableValue(idxPar, parValues[idxPar]);
-  }
-
-  mlfit->FixVariable(parToScan);
-
-  TGraph* graph = new TGraph(numPoints);
-  for ( size_t idxPoint = 0; idxPoint < numPoints; ++idxPoint )
-  {
-    double x = xMin + idxPoint*dx;
-    mlfit->SetFixedVariable(parToScan, parName.c_str(), x);
-    mlfit->Minimize();
-    double y = mlfit->MinValue();
-    std::cout << " point #" << idxPoint << ": x = " << x << ", y = " << y - yMin << "\n";
-    graph->SetPoint(idxPoint, x, y - yMin);
-  }
-  return graph;
-}
-
-void showGraphs(double canvasSizeX, double canvasSizeY,
-                TGraph* graph1, const std::string& legendEntry1,
-                TGraph* graph2, const std::string& legendEntry2,
-                double legendTextSize, double legendPosX, double legendPosY, double legendSizeX, double legendSizeY, 
-                double xMin, double xMax, const std::string& xAxisTitle, double xAxisOffset,
-                bool useLogScale, double yMin, double yMax, const std::string& yAxisTitle, double yAxisOffset,
-                const std::string& outputFileName)
-{
-  TCanvas* canvas = new TCanvas("canvas", "canvas", canvasSizeX, canvasSizeY);
-  canvas->SetFillColor(10);
-  canvas->SetBorderSize(2);
-  canvas->SetTopMargin(0.05);
-  canvas->SetLeftMargin(0.14);
-  canvas->SetBottomMargin(0.12);
-  canvas->SetRightMargin(0.05);
-  canvas->SetLogy(useLogScale);
-  
-  canvas->SetGridx(1);
-  canvas->SetGridy(1);
-
-  if ( !graph1 ) 
-  {
-    std::cerr << "<showGraphs>: graph1 = NULL --> skipping !!" << std::endl;
-    return;
-  }
-
-  TH1* dummyHistogram = new TH1F("dummyHistogram", "dummyHistogram", 10, xMin, xMax);
-  dummyHistogram->SetTitle("");
-  dummyHistogram->SetStats(false);
-  dummyHistogram->SetMinimum(yMin);
-  dummyHistogram->SetMaximum(yMax);
-
-  TAxis* xAxis = dummyHistogram->GetXaxis();
-  xAxis->SetTitle(xAxisTitle.data());
-  xAxis->SetTitleSize(0.045);
-  xAxis->SetTitleOffset(xAxisOffset);  
-  if ( xMax > xMin ) 
-  {
-    std::cout << "limiting x-axis range to " << xMin << ".." << xMax << std::endl;
-    xAxis->SetRangeUser(xMin, xMax);
-  }
-
-  TAxis* yAxis = dummyHistogram->GetYaxis();
-  yAxis->SetTitle(yAxisTitle.data());
-  yAxis->SetTitleSize(0.045);
-  yAxis->SetTitleOffset(yAxisOffset);
-
-  dummyHistogram->Draw();
-  //dummyHistogram->Draw("axis");
-
-  graph1->SetMarkerColor(1);
-  graph1->SetMarkerSize(1);
-  graph1->SetMarkerStyle(8);
-  graph1->SetLineColor(1);
-  graph1->SetLineWidth(1);
-  graph1->SetLineStyle(1);
-  graph1->Draw("PL");
-
-  if ( graph2 ) 
-  {
-    graph2->SetMarkerColor(2);
-    graph2->SetMarkerSize(1);
-    graph2->SetMarkerStyle(8);
-    graph2->SetLineColor(2);
-    graph2->SetLineWidth(1);
-    graph2->SetLineStyle(1);
-    graph2->Draw("PL");
-  }
-
-  TLegend* legend = 0;
-  if ( legendEntry1 != "" ) 
-  {
-    legend = new TLegend(legendPosX, legendPosY, legendPosX + legendSizeX, legendPosY + legendSizeY, "", "brNDC"); 
-    legend->SetBorderSize(0);
-    legend->SetFillColor(0);
-    legend->SetTextSize(legendTextSize);
-    legend->AddEntry(graph1, legendEntry1.data(), "L");
-    if ( graph2 ) legend->AddEntry(graph2, legendEntry2.data(), "L");
-    legend->Draw();
-  }
-
-  dummyHistogram->Draw("axissame");
-
-  canvas->RedrawAxis();
-
-  canvas->Update();
-  std::string outputFileName_plot = "plots/";
-  size_t idx = outputFileName.find_last_of('.');
-  outputFileName_plot.append(std::string(outputFileName, 0, idx));
-  //if ( idx != std::string::npos ) canvas->Print(std::string(outputFileName_plot).append(std::string(outputFileName, idx)).data());
-  canvas->Print(std::string(outputFileName_plot).append(".png").data());
-  //canvas->Print(std::string(outputFileName_plot).append(".pdf").data());
-
-  delete dummyHistogram;
-  delete legend;
-  delete canvas;  
-}
-
-Measurement
-comp_Bp_Bm_C_by_mlfit(const EntanglementDataset& dataset, ROOT::Math::Minimizer* mlfit, 
-                      bool scanLikelihood = false, const std::string& outputFileName = "", 
-                      int verbosity = -1)
-{
-  // initialize fit parameters
-  Measurement startpos = comp_Bp_Bm_C_by_summation(dataset);
-  const math::Matrix3x3& startpos_C = startpos.get_C();
-  std::map<size_t, std::string> parNames = get_parNames();
-  size_t npar = parNames.size();
-  for ( size_t idxPar = 0; idxPar < npar; ++idxPar )
-  {
-    double par0 = 0.;
-    if ( idxPar >= 6 && idxPar <= 14 )
-    {
-      size_t idxRow = (idxPar - 6) / 3;
-      size_t idxCol = (idxPar - 6) % 3;
-      par0 = startpos_C(idxRow,idxCol);
-    }
-    mlfit->SetLimitedVariable(idxPar, parNames[idxPar].c_str(), par0, 0.1, -2., +2.);
-  }
-
-  gEntanglementDataset = &dataset;
-
-  mlfit->Minimize();
-  mlfit->Hesse();
-
-  if ( verbosity >= 1 )
-  {
-    std::cout << "Fit Results:\n";
-    mlfit->PrintResults();
-  }
-
-  std::vector<double> parValues(npar);
-  std::vector<double> parErrors(npar);
-  for ( size_t idxPar = 0; idxPar < npar; ++idxPar )
-  {
-    const double* X = mlfit->X();
-    parValues[idxPar] = X[idxPar];
-    parErrors[idxPar] = sqrt(mlfit->CovMatrix(idxPar, idxPar));
-    if ( verbosity >= 1 )
-    {
-      std::cout << parNames[idxPar] << " = " << parValues[idxPar] << " +/- " << parErrors[idxPar] << "\n";
-    }
-  }
-
-  if ( scanLikelihood )
-  {
-    double mlfitMin = mlfit->MinValue();
-    std::map<size_t, std::string> parNames = get_parNames();
-    std::vector<size_t> parsToScan = { 6, 10, 14 };
-    for ( size_t i = 0; i < parsToScan.size(); ++i )
-    {
-      size_t parToScan = parsToScan[i];      
-      std::cout << "Scanning likelihood as function of parameter " << parNames[parToScan] << "...\n";
-      double xMin = parValues[parToScan] - std::max(0.01, 5.*parErrors[parToScan]);
-      double xMax = parValues[parToScan] + std::max(0.01, 5.*parErrors[parToScan]);
-      TGraph* mlfitScan_fixed = scan_mlfit_fixed(parValues, parToScan, parNames[parToScan], 100, xMin, xMax, mlfitMin);
-      TGraph* mlfitScan_profiled = scan_mlfit_profiled(mlfit, parValues, parToScan, parNames[parToScan], 100, xMin, xMax, mlfitMin);
-      std::string outputFileName_plot = (const char*)TString(outputFileName.c_str()).ReplaceAll(".root", Form("_mlfitScan_%s.png", parNames[parToScan].c_str()));
-      showGraphs(1150, 950,
-                 mlfitScan_fixed,    "Fixed",
-                 mlfitScan_profiled, "Profiled",
-                 0.040, 0.48, 0.78, 0.22, 0.14, 
-	         xMin, xMax, parNames[parToScan], 1.2,
-                 false, 0., 2.5e+1, "-2 log(L)", 1.4,
-                 outputFileName_plot);
-      std::cout << " Done.\n";
-    }
-  }
-
-  math::Vector3 Bp;
-  math::Vector3 BpErr;
-  math::Vector3 Bm;
-  math::Vector3 BmErr;
-  math::Matrix3x3 C;
-  math::Matrix3x3 CErr;
-  for ( size_t idxPar = 0; idxPar < npar; ++idxPar )
-  {
-    if ( idxPar <= 2 )
-    {
-      size_t idx = idxPar;
-      Bp[idx] = parValues[idxPar];
-      BpErr[idx] = parErrors[idxPar];
-    }
-    else if ( idxPar <= 5 )
-    {
-      size_t idx = idxPar - 3;
-      Bm[idx] = parValues[idxPar];
-      BmErr[idx] = parErrors[idxPar];
-    }
-    else if ( idxPar <= 14 )
-    {	
-      size_t idxRow = (idxPar - 6) / 3;
-      size_t idxCol = (idxPar - 6) % 3;
-      C[idxRow][idxCol] = parValues[idxPar];
-      CErr[idxRow][idxCol] = parErrors[idxPar];
-    } else assert(0);
-  }
 
   Measurement measurement(Bp, BpErr, Bm, BmErr, C, CErr, verbosity);
   return measurement;
@@ -1030,67 +664,31 @@ int main(int argc, char* argv[])
 
   EntanglementDataset nominal_sample(dataset, maxEvents_afterCuts);
 
-  Measurement nominal_measurement_by_summation = comp_Bp_Bm_C_by_summation(nominal_sample, verbosity);
+  Measurement nominal_measurement = comp_Bp_Bm_C(nominal_sample, verbosity);
  
-  // initialize Minuit
-  ROOT::Math::Minimizer* mlfit = new ROOT::Minuit2::Minuit2Minimizer();
-  mlfit->SetMaxFunctionCalls(10000);
-  mlfit->SetTolerance(1.e-3);
-  mlfit->SetPrintLevel(-1);
-
-  // set function pointer
-  ROOT::Math::Functor f(&mlfit_fcn, npar);
-  mlfit->SetFunction(f);
-
-  Measurement nominal_measurement_by_mlfit = comp_Bp_Bm_C_by_mlfit(nominal_sample, mlfit, scanLikelihood, outputFile.file(), verbosity);
-
   // CV: estimate uncertainties on Bp and Bm vectors, on tau spin correlation matrix C,
   //     and on Entanglement observables with bootstrap samples
-  std::vector<Measurement> bootstrap_measurements_by_summation;
-  std::vector<Measurement> bootstrap_measurements_by_mlfit;
+  std::vector<Measurement> bootstrap_measurements;
   for ( size_t idxBootstrapSample = 0; idxBootstrapSample < numBootstrapSamples; ++idxBootstrapSample )
   {
     EntanglementDataset bootstrap_sample = build_bootstrap_sample(dataset, rnd, maxEvents_afterCuts);
-  
-    Measurement measurement_by_summation = comp_Bp_Bm_C_by_summation(bootstrap_sample, -1);
-    bootstrap_measurements_by_summation.push_back(measurement_by_summation);
-
-    Measurement measurement_by_mlfit = comp_Bp_Bm_C_by_mlfit(bootstrap_sample, mlfit, false, "", -1);
-    bootstrap_measurements_by_mlfit.push_back(measurement_by_mlfit);
+    Measurement measurement = comp_Bp_Bm_C(bootstrap_sample, -1);
+    bootstrap_measurements.push_back(measurement);
   }
 
-  math::Vector3 Bp_median_by_summation, BpErr_by_summation, Bm_median_by_summation, BmErr_by_summation;
-  math::Matrix3x3 C_median_by_summation, CErr_by_summation;
-  double Rchsh_median_by_summation, RchshErr_by_summation;
-  comp_median_and_Err(bootstrap_measurements_by_summation, 
-    Bp_median_by_summation, BpErr_by_summation,
-    Bm_median_by_summation, BmErr_by_summation,
-    C_median_by_summation, CErr_by_summation,
-    Rchsh_median_by_summation, RchshErr_by_summation);
+  math::Vector3 Bp_median, BpErr, Bm_median, BmErr;
+  math::Matrix3x3 C_median, CErr;
+  double Rchsh_median, RchshErr;
+  comp_median_and_Err(bootstrap_measurements, 
+    Bp_median, BpErr,
+    Bm_median, BmErr,
+    C_median, CErr,
+    Rchsh_median, RchshErr);
   std::cout << "Matrix C (measured using Eq. (25) of arXiv:2211.10513):\n";
-  std::cout << nominal_measurement_by_summation.get_C() << "\n";
+  std::cout << nominal_measurement.get_C() << "\n";
   std::cout << "+/-\n";
-  std::cout << CErr_by_summation << "\n";
-  std::cout << "Rchsh = " << nominal_measurement_by_summation.get_Rchsh() << " +/- " << RchshErr_by_summation << "\n";
-
-  math::Vector3 Bp_median_by_mlfit, BpErr_by_mlfit, Bm_median_by_mlfit, BmErr_by_mlfit;
-  math::Matrix3x3 C_median_by_mlfit, CErr_by_mlfit;
-  double Rchsh_median_by_mlfit, RchshErr_by_mlfit;
-  comp_median_and_Err(bootstrap_measurements_by_mlfit, 
-    Bp_median_by_mlfit, BpErr_by_mlfit,
-    Bm_median_by_mlfit, BmErr_by_mlfit,
-    C_median_by_mlfit, CErr_by_mlfit,
-    Rchsh_median_by_mlfit, RchshErr_by_mlfit);
-  std::cout << "Matrix C (measured using maximum-likelihood fit):\n";
-  std::cout << nominal_measurement_by_mlfit.get_C() << "\n";
-  std::cout << "+/-\n";
-  std::cout << CErr_by_mlfit << "\n";
-  if ( verbosity >= 1 )
-  {
-    std::cout << "Uncertainty estimated by maximum-likelihood fit (for comparison):\n";
-    std::cout << nominal_measurement_by_mlfit.get_CErr() << "\n";
-  }
-  std::cout << "Rchsh = " << nominal_measurement_by_mlfit.get_Rchsh() << " +/- " << RchshErr_by_mlfit << "\n";
+  std::cout << CErr << "\n";
+  std::cout << "Rchsh = " << nominal_measurement.get_Rchsh() << " +/- " << RchshErr << "\n";
 
   if ( verbosity >= 1 )
   {
@@ -1101,8 +699,6 @@ int main(int argc, char* argv[])
     C_exp[2][2] = -1.;
     C_exp.Print();
   }
-
-  delete mlfit;
 
   clock.Show("analyzeEntanglementNtuple");
 
