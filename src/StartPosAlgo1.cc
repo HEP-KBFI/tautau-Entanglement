507d4d35e4ee815a60bb6bcf63fced62439228e5
Christian Veelken <christian.veelken@cern.ch>
2023-08-24 11:21:58 +0300

    fixed a few bugs in the Event reconstruction based on Appendix C of the paper arXiv:2211.10513

--- src/StartPosAlgo1.cc
+++ src/StartPosAlgo1.cc
@@ -66,9 +66,12 @@ namespace
   }
 
   double
-  get_epsilon(int mu, int nu, int rho, int sigma)
+  get_epsilon(int mu, int nu, int rho, int sigma, int verbosity)
   {
-    //std::cout << "<get_epsilon>:\n";
+    //if ( verbosity >= 3 )
+    //{
+    //  std::cout << "<get_epsilon>:\n";
+    //}
     // CV: formula for computation of four-dimensional Levi-Civita symbol taken from
     //       https://en.wikipedia.org/wiki/Levi-Civita_symbol
     //    (Section "Definition" -> "Generalization to n dimensions")
@@ -85,8 +88,11 @@ namespace
         epsilon *= sgn(a[j] - a[i]); 
       }
     }
-    //std::cout << " mu = " << mu << ", nu = " << nu << ", rho = " << rho << ", sigma = " << sigma << ":" 
-    //          << " epsilon = " << epsilon << "\n";
+    //if ( verbosity >= 3 )
+    //{
+    //  std::cout << " mu = " << mu << ", nu = " << nu << ", rho = " << rho << ", sigma = " << sigma << ":" 
+    //            << " epsilon = " << epsilon << "\n";
+    //}
     return epsilon;
   }
 
@@ -98,6 +104,13 @@ namespace
          reco::Candidate::LorentzVector& qP4,
          int verbosity, bool cartesian = true)
   {
+    if ( verbosity >= 3 )
+    {
+      std::cout << "<comp_d>:\n";
+      printLorentzVector("p_H", higgsP4);
+      printLorentzVector("p_vis+", visTauPlusP4);
+      printLorentzVector("p_vis-", visTauMinusP4);
+    }
     double a2 = square(a);
     double b2 = square(b);
     double c2 = square(c);
@@ -119,23 +132,32 @@ namespace
           for ( int sigma = 0; sigma < 4; ++sigma )
           {
             double visTauMinusP4_sigma = get_component(visTauMinusP4, sigma);
-            // CV: minus sign on four-dimensional Levi-Civita symbol 
-            //     due to raising indices from covariant from contravariant from
-            //     by multiplication with metric tensor (+---),
+            // CV: compute Levi-Civita symbol in Eq. (C3) of the paper arXiv:2211.10513,
             //    cf. https://en.wikipedia.org/wiki/Levi-Civita_symbol
             //    (Section "Levi-Civita tensors")
-            double epsilon = -get_epsilon(mu, nu, rho, sigma);
+            double epsilon = get_epsilon(mu, nu, rho, sigma, verbosity);
             double product = (1./mH2)*epsilon*higgsP4_nu*visTauPlusP4_rho*visTauMinusP4_sigma;
-            if      ( mu == 0 ) qE  += product;
-            else if ( mu == 1 ) qPx += product;
-            else if ( mu == 2 ) qPy += product;
-            else if ( mu == 3 ) qPz += product;
+            // CV: minus sign for spatial dimensions 
+            //     due to raising indices from covariant from contravariant from
+            //     by multiplication with metric tensor (+---)
+            double sign = ( mu == 0 ) ? +1. : -1.;            
+            if      ( mu == 0 ) qE  += sign*product;
+            else if ( mu == 1 ) qPx += sign*product;
+            else if ( mu == 2 ) qPy += sign*product;
+            else if ( mu == 3 ) qPz += sign*product;
             else assert(0);
           }
         }
       }
     }
     qP4 = reco::Candidate::LorentzVector(qPx, qPy, qPz, qE);
+    if ( verbosity >= 3 )
+    {
+      printLorentzVector("q", qP4);
+      std::cout << "q*p_H = " << qP4.Dot(higgsP4) << "\n";
+      std::cout << "q*p_vis+ = " << qP4.Dot(visTauPlusP4) << "\n";
+      std::cout << "q*p_vis- = " << qP4.Dot(visTauMinusP4) << "\n";
+    }
     // CV: compute q2 "by hand" instead of calling qP4.mass() function,
     //     as the four-vector qP4 may be space-like, i.e. have negative "mass"
     double q2 = qE*qE - (qPx*qPx + qPy*qPy + qPz*qPz);
@@ -147,7 +169,7 @@ namespace
 
     const double mTau2 = square(mTau);
 
-    double d2 = (-0.25/q2)*((1. + a2)*mH2 + 0.5*(b2 + c2)*(mVisTauPlus2 + mVisTauMinus2) - 4.*mTau2 + 2.*(a*c*y - a*b*x - b*c*z));
+    double d2 = (-0.25/q2)*((1. + a2)*mH2 + b2*mVisTauPlus2 + c2*mVisTauMinus2 - 4.*mTau2 + 2.*(a*c*y - a*b*x - b*c*z));
     if ( verbosity >= 1 )
     {
       std::cout << "d2 = " << d2 << "\n";
@@ -178,73 +200,9 @@ namespace
     double tipPerp = std::fabs(e_perp.Dot(flightlength));
     return tipPerp;
   }
-
-  std::pair<reco::Candidate::LorentzVector, reco::Candidate::LorentzVector>
-  comp_tauP4(double a, double b, double c, const std::vector<double>& d,
-             const reco::Candidate::LorentzVector& higgsP4,
-             const reco::Candidate::LorentzVector& visTauPlusP4,
-             const reco::Candidate::LorentzVector& visTauMinusP4,
-             const reco::Candidate::LorentzVector& qP4,
-             const KinematicEvent& evt,
-             double& tauMassFix,
-             int verbosity, bool cartesian = true)
-  {
-    reco::Candidate::LorentzVector tauPlusP4_min_tipPerp;
-    reco::Candidate::LorentzVector tauMinusP4_min_tipPerp;
-    double min_tipPerp = 1.e+3;
-    for ( size_t idx = 0; idx < d.size(); ++idx )
-    {
-      reco::Candidate::LorentzVector tauPlusP4  = 0.5*(1. - a)*higgsP4 + 0.5*b*visTauPlusP4 - 0.5*c*visTauMinusP4 + d[idx]*qP4;
-      reco::Candidate::LorentzVector tauMinusP4 = 0.5*(1. + a)*higgsP4 - 0.5*b*visTauPlusP4 + 0.5*c*visTauMinusP4 - d[idx]*qP4;
-      double tipPerp = get_tipPerp(tauPlusP4, visTauPlusP4, evt.pv(), evt.tipPCATauPlus())
-                      + get_tipPerp(tauMinusP4, visTauMinusP4, evt.pv(), evt.tipPCATauMinus());
-      if ( verbosity >= 1 )
-      {
-        std::cout << "solution #" << idx << ": tipPerp = " << tipPerp << "\n";
-        printLorentzVector("tauPlusP4", tauPlusP4, cartesian);
-        if ( cartesian )
-        {
-          std::cout << " mass = " << tauPlusP4.mass() << "\n";
-        }
-        printLorentzVector("tauMinusP4", tauMinusP4, cartesian);
-        {
-          std::cout << " mass = " << tauMinusP4.mass() << "\n";
-        }
-      }
-      if ( idx == 0 || tipPerp < min_tipPerp )
-      {        
-        // CV: the reconstruction of the tau+ and tau- four-vectors using Eq. (C3)
-        //     of the paper arXiv:2211.10513 yields mass values that a few GeV off,
-        //     presumably due to rounding errors.
-        //     Adjust the energy component of the tau+ and tau- four-vectors 
-        //     such that their mass values match the correct (PDG) value,
-        //     while keeping the Px, Py, Pz momentum components fixed
-        tauPlusP4_min_tipPerp = fixTauMass(tauPlusP4);
-        tauMinusP4_min_tipPerp = fixTauMass(tauMinusP4);
-        min_tipPerp = tipPerp;
-        tauMassFix = square(tauPlusP4.mass() - mTau) + square(tauMinusP4.mass() - mTau);
-      }
-    }
-    if ( verbosity >= 1 )
-    {
-      std::cout << "best solution:\n";
-      printLorentzVector("tauPlusP4", tauPlusP4_min_tipPerp, cartesian);
-      if ( cartesian )
-      {
-        std::cout << " mass = " << tauPlusP4_min_tipPerp.mass() << "\n";
-      }
-      printLorentzVector("tauMinusP4", tauMinusP4_min_tipPerp, cartesian);
-      if ( cartesian )
-      {
-        std::cout << " mass = " << tauMinusP4_min_tipPerp.mass() << "\n";
-      }
-    }
-
-    return std::make_pair(tauPlusP4_min_tipPerp, tauMinusP4_min_tipPerp);
-  }
 }
 
-KinematicEvent
+std::vector<KinematicEvent>
 StartPosAlgo1::operator()(const KinematicEvent& kineEvt)
 {
   if ( verbosity_ >= 1 )
@@ -343,7 +301,7 @@ StartPosAlgo1::operator()(const KinematicEvent& kineEvt)
   M(2,2) =  y;
   if ( verbosity_ >= 1 )
   {
-    std::cout << "M0:\n";
+    std::cout << "M:\n";
     std::cout << M << "\n";
   }
 
@@ -355,7 +313,7 @@ StartPosAlgo1::operator()(const KinematicEvent& kineEvt)
   lambda(2) = 0.;
   if ( verbosity_ >= 1 )
   {
-    std::cout << "lambda0:\n";
+    std::cout << "lambda:\n";
     std::cout << lambda << "\n";
   }
 
@@ -369,148 +327,73 @@ StartPosAlgo1::operator()(const KinematicEvent& kineEvt)
 
   math::Vector3 v = Minv*lambda;
 
-  double a0 = v(0);
-  double b0 = v(1);
-  double c0 = v(2);
+  double a = v(0);
+  double b = v(1);
+  double c = v(2);
   if ( verbosity_ >= 1 )
   {
-    std::cout << "a0 = " << a0 << "\n";
-    std::cout << "b0 = " << b0 << "\n";
-    std::cout << "c0 = " << c0 << "\n";
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+    std::cout << "c = " << c << "\n";
   }
 
-  reco::Candidate::LorentzVector q0P4;
-  std::vector<double> d0 = comp_d(a0, b0, c0, x, y, z, higgsP4, visTauPlusP4, mVisTauPlus2, visTauMinusP4, mVisTauMinus2, q0P4, verbosity_, cartesian_);
-  double tauMassFix0 = 0.;
-  auto tau0P4 = comp_tauP4(a0, b0, c0, d0, higgsP4, visTauPlusP4, visTauMinusP4, q0P4, kineEvt, tauMassFix0, verbosity_, cartesian_);
-  reco::Candidate::LorentzVector tauPlusP4 = tau0P4.first;
-  reco::Candidate::LorentzVector tauMinusP4 = tau0P4.second;
+  std::vector<KinematicEvent> kineEvts_startpos;
 
-  int iteration = 0;
-  const int max_iterations = 10;
-  bool hasConverged = false;
-  while ( !hasConverged && iteration < max_iterations )
+  reco::Candidate::LorentzVector qP4;
+  std::vector<double> d = comp_d(a, b, c, x, y, z, higgsP4, visTauPlusP4, mVisTauPlus2, visTauMinusP4, mVisTauMinus2, qP4, verbosity_, cartesian_);
+  for ( size_t idx = 0; idx < d.size(); ++idx )
   {
+    reco::Candidate::LorentzVector tauPlusP4  = 0.5*(1. - a)*higgsP4 + 0.5*b*visTauPlusP4 - 0.5*c*visTauMinusP4 + d[idx]*qP4;
+    reco::Candidate::LorentzVector tauMinusP4 = 0.5*(1. + a)*higgsP4 - 0.5*b*visTauPlusP4 + 0.5*c*visTauMinusP4 - d[idx]*qP4;
     if ( verbosity_ >= 1 )
     {
-      std::cout << "iteration #" << iteration << ":\n";
+      std::cout << "solution #" << idx << ":\n";
+      printLorentzVector("tauPlusP4", tauPlusP4, cartesian_);
+      if ( cartesian_ )
+      {
+        std::cout << " mass = " << tauPlusP4.mass() << "\n";
+      }
+      printLorentzVector("tauMinusP4", tauMinusP4, cartesian_);
+      {
+        std::cout << " mass = " << tauMinusP4.mass() << "\n";
+      }
+      double tipPerp = get_tipPerp(tauPlusP4, visTauPlusP4, kineEvt.pv(), kineEvt.tipPCATauPlus())
+                      + get_tipPerp(tauMinusP4, visTauMinusP4, kineEvt.pv(), kineEvt.tipPCATauMinus());
+      std::cout << "tipPerp = " << tipPerp << "\n";
     }
 
-    double dm2 = mVisTauMinus2 - mVisTauPlus2;
+    KinematicEvent kineEvt_startpos = kineEvt;
 
-    M(0,0) = -x;
-    M(0,1) = mVisTauPlus2;
-    M(0,2) = -z;
-    M(1,0) =  y;
-    M(1,1) = -z;
-    M(1,2) = mVisTauMinus2;
-    M(2,0) = mH2;
-    M(2,1) = -x + 0.5*b0*dm2;
-    M(2,2) =  y + 0.5*c0*dm2;
-    if ( verbosity_ >= 1 )
+    kineEvt_startpos.tauPlusP4_ = tauPlusP4;
+    kineEvt_startpos.tauPlusP4_isValid_ = true;
+    //kineEvt_startpos.nuTauPlusP4_ = fixNuMass(tauPlusP4 - visTauPlusP4);
+    kineEvt_startpos.nuTauPlusP4_ = tauPlusP4 - visTauPlusP4;
+    kineEvt_startpos.nuTauPlusP4_isValid_ = true;
+    if ( is1Prong(kineEvt_startpos.tauPlus_decayMode()) )
     {
-      std::cout << "M:\n";
-      std::cout << M << "\n";
-    }
-  
-    lambda(0) = mTau2 + mVisTauPlus2  - x;
-    lambda(1) = mTau2 + mVisTauMinus2 - y;
-    lambda(2) = 0.25*(square(b0) + square(c0))*dm2;
-    if ( verbosity_ >= 1 )
-    {
-      std::cout << "lambda:\n";
-      std::cout << lambda << "\n";
+      const KinematicParticle* tauPlus_leadTrack = get_leadTrack(kineEvt_startpos.daughtersTauPlus());
+      assert(tauPlus_leadTrack);
+      const reco::Candidate::Point& tauPlus_tipPCA = kineEvt_startpos.tipPCATauPlus();
+      kineEvt_startpos.svTauPlus_ = comp_PCA_line2line(kineEvt_startpos.pv(), tauPlusP4, tauPlus_tipPCA, tauPlus_leadTrack->p4(), verbosity_);
+      kineEvt_startpos.svTauPlus_isValid_ = true;
     }
  
-    v = Minv*lambda;
-
-    double a1 = v(0);
-    double b1 = v(1);
-    double c1 = v(2);
-    if ( verbosity_ >= 1 )
+    kineEvt_startpos.tauMinusP4_ = tauMinusP4;
+    kineEvt_startpos.tauMinusP4_isValid_ = true;
+    //kineEvt_startpos.nuTauMinusP4_ = fixNuMass(tauMinusP4 - visTauMinusP4);
+    kineEvt_startpos.nuTauMinusP4_ = tauMinusP4 - visTauMinusP4;
+    kineEvt_startpos.nuTauMinusP4_isValid_ = true;
+    if ( is1Prong(kineEvt_startpos.tauMinus_decayMode()) )
     {
-      std::cout << "a = " << a1 << "\n";
-      std::cout << "b = " << b1 << "\n";
-      std::cout << "c = " << c1 << "\n";
+      const KinematicParticle* tauMinus_leadTrack = get_leadTrack(kineEvt_startpos.daughtersTauMinus());
+      assert(tauMinus_leadTrack);
+      const reco::Candidate::Point& tauMinus_tipPCA = kineEvt_startpos.tipPCATauMinus();
+      kineEvt_startpos.svTauMinus_ = comp_PCA_line2line(kineEvt_startpos.pv(), tauMinusP4, tauMinus_tipPCA, tauMinus_leadTrack->p4(), verbosity_);
+      kineEvt_startpos.svTauMinus_isValid_ = true;
     }
 
-    reco::Candidate::LorentzVector q1P4;
-    std::vector<double> d1 = comp_d(a1, b1, c1, x, y, z, higgsP4, visTauPlusP4, mVisTauPlus2, visTauMinusP4, mVisTauMinus2, q1P4, verbosity_, cartesian_);
-    double tauMassFix1 = 0.;
-    auto tau1P4 = comp_tauP4(a1, b1, c1, d1, higgsP4, visTauPlusP4, visTauMinusP4, q1P4, kineEvt, tauMassFix1, verbosity_, cartesian_);
-    if ( verbosity_ >= 1 )
-    {
-      std::cout << "tauMassFix1 = " << tauMassFix1 << " (tauMassFix0 = " << tauMassFix0 << ")\n";
-    }
-
-    if ( tauMassFix1 < tauMassFix0 )
-    {
-      a0 = a1;
-      b0 = b1;
-      c0 = c1;
-      tau0P4 = tau1P4;
-      tauPlusP4 = tau1P4.first;
-      tauMinusP4 = tau1P4.second;
-      tauMassFix0 = tauMassFix1;
-    }
-    else
-    {
-      hasConverged = true;
-    }
-    ++iteration;
-  }
-  if ( !hasConverged )
-  {
-    std::cerr << "WARNING: StartPosAlgo1 failed to converge !!" << std::endl;
-  }
-  if ( verbosity_ >= 1 )
-  {
-    std::cout << "#iterations = " << iteration << " (max_iterations = " << max_iterations << ")\n";
+    kineEvts_startpos.push_back(kineEvt_startpos);
   }
 
-  if ( verbosity_ >= 1 )
-  {
-    std::cout << "final start position for kinematic fit:\n";
-    printLorentzVector("tauPlusP4", tauPlusP4, cartesian_);
-    if ( cartesian_ )
-    {
-      std::cout << " mass = " << tauPlusP4.mass() << "\n";
-    }
-    printLorentzVector("tauMinusP4", tauMinusP4, cartesian_);
-    {
-      std::cout << " mass = " << tauMinusP4.mass() << "\n";
-    }
-  }
-
-  KinematicEvent kineEvt_startpos = kineEvt;
-
-  kineEvt_startpos.tauPlusP4_ = tauPlusP4;
-  kineEvt_startpos.tauPlusP4_isValid_ = true;
-  //kineEvt_startpos.nuTauPlusP4_ = fixNuMass(tauPlusP4 - visTauPlusP4);
-  kineEvt_startpos.nuTauPlusP4_ = tauPlusP4 - visTauPlusP4;
-  kineEvt_startpos.nuTauPlusP4_isValid_ = true;
-  if ( is1Prong(kineEvt_startpos.tauPlus_decayMode()) )
-  {
-    const KinematicParticle* tauPlus_leadTrack = get_leadTrack(kineEvt_startpos.daughtersTauPlus());
-    assert(tauPlus_leadTrack);
-    const reco::Candidate::Point& tauPlus_tipPCA = kineEvt_startpos.tipPCATauPlus();
-    kineEvt_startpos.svTauPlus_ = comp_PCA_line2line(kineEvt_startpos.pv(), tauPlusP4, tauPlus_tipPCA, tauPlus_leadTrack->p4(), verbosity_);
-    kineEvt_startpos.svTauPlus_isValid_ = true;
-  }
-
-  kineEvt_startpos.tauMinusP4_ = tauMinusP4;
-  kineEvt_startpos.tauMinusP4_isValid_ = true;
-  //kineEvt_startpos.nuTauMinusP4_ = fixNuMass(tauMinusP4 - visTauMinusP4);
-  kineEvt_startpos.nuTauMinusP4_ = tauMinusP4 - visTauMinusP4;
-  kineEvt_startpos.nuTauMinusP4_isValid_ = true;
-  if ( is1Prong(kineEvt_startpos.tauMinus_decayMode()) )
-  {
-    const KinematicParticle* tauMinus_leadTrack = get_leadTrack(kineEvt_startpos.daughtersTauMinus());
-    assert(tauMinus_leadTrack);
-    const reco::Candidate::Point& tauMinus_tipPCA = kineEvt_startpos.tipPCATauMinus();
-    kineEvt_startpos.svTauMinus_ = comp_PCA_line2line(kineEvt_startpos.pv(), tauMinusP4, tauMinus_tipPCA, tauMinus_leadTrack->p4(), verbosity_);
-    kineEvt_startpos.svTauMinus_isValid_ = true;
-  }
-
-  return kineEvt_startpos;
+  return kineEvts_startpos;
 }
