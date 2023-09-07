a25b0225770b2584918b15de58887703cd5782de
Christian Veelken <christian.veelken@cern.ch>
2023-09-07 11:15:34 +0300

    moved computation of Bp, Bm, and C into separate function for better code reuse

---
+++ interface/comp_BandC.h
@@ -0,0 +1,19 @@
+#ifndef TauAnalysis_Entanglement_comp_BandC_h
+#define TauAnalysis_Entanglement_comp_BandC_h
+
+#include "TauAnalysis/Entanglement/interface/Matrix_and_Vector.h" // math::Matrix3x3, math::Vector3
+
+math::Vector3
+comp_Bp(double hPlus_n, double hPlus_r, double hPlus_k,
+        double evtWeight = 1.);
+
+math::Vector3
+comp_Bm(double hMinus_n, double hMinus_r, double hMinus_k,
+        double evtWeight = 1.);
+
+math::Matrix3x3
+comp_C(double hPlus_n, double hPlus_r, double hPlus_k,
+       double hMinus_n, double hMinus_r, double hMinus_k,
+       double evtWeight = 1.);
+
+#endif // TauAnalysis_Entanglement_comp_BandC_h
