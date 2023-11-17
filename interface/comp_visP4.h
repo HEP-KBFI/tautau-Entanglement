e104f953f510a006d54e221385972c7265efb744
Christian Veelken <christian.veelken@cern.ch>
2023-11-17 16:41:05 +0200

    added option to compute visible momentum for KinematicParticles

--- interface/comp_visP4.h
+++ interface/comp_visP4.h
@@ -1,15 +1,20 @@
 #ifndef TauAnalysis_Entanglement_comp_visP4_h
 #define TauAnalysis_Entanglement_comp_visP4_h
 
-#include "DataFormats/Candidate/interface/Candidate.h"        // reco::Candidate::LorentzVector
+#include "DataFormats/Candidate/interface/Candidate.h"            // reco::Candidate::LorentzVector
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow"
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h" // reco::GenParticle
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"     // reco::GenParticle
 #pragma GCC diagnostic pop
 
-#include <vector>                                             // std::vector<>
+#include "TauAnalysis/Entanglement/interface/KinematicParticle.h" // KinematicParticle
+
+#include <vector>                                                 // std::vector<>
 
 reco::Candidate::LorentzVector
 comp_visP4(const std::vector<const reco::GenParticle*>& decayProducts);
+
+reco::Candidate::LorentzVector
+comp_visP4(const std::vector<KinematicParticle>& decayProducts);
 
 #endif // TauAnalysis_Entanglement_comp_visP4_h
--- src/comp_visP4.cc
+++ src/comp_visP4.cc
@@ -12,3 +12,16 @@ comp_visP4(const std::vector<const reco::GenParticle*>& decayProducts)
   }
   return visP4;
 }
+
+reco::Candidate::LorentzVector
+comp_visP4(const std::vector<KinematicParticle>& decayProducts)
+{
+  reco::Candidate::LorentzVector visP4;
+  for ( const KinematicParticle& decayProduct : decayProducts )
+  {
+    int absPdgId = std::abs(decayProduct.pdgId());
+    if ( absPdgId == 12 || absPdgId == 14 || absPdgId == 16 ) continue;
+    visP4 += decayProduct.p4();
+  }
+  return visP4;
+}
