49c6e7335bb7cf84caf19d2040e3fc0a77d49445
ktht <karlehataht@gmail.com>
2024-01-12 15:06:58 +0200

    Update the sample dictionary since the mounting point has changed

--- python/samples.py
+++ python/samples.py
@@ -11,22 +11,22 @@ MAX_SUM_PHOTON_EN_SUPERKEKB = 0.2
 # CV: define samples for tau spin analysis @ LHC
 samples_LHC = {
   'ggH_htt_pythia8' : {
-    'inputFilePath' : '/store/mc/RunIISummer20UL18MiniAODv2/GluGluHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v3/100000/',
+    'inputFilePath' : '/cms/store/mc/RunIISummer20UL18MiniAODv2/GluGluHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v3/100000/',
     'numJobs' : 10,
     'apply_evtWeight' : True
   },
   'ggH_htt_tauola' : {
-   'inputFilePath' : '/store/mc/RunIISummer16MiniAODv2/GluGluToHToTauTau_M125_13TeV_amcatnloFXFX_pythia8_tauola/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/',
+   'inputFilePath' : '/cms/store/mc/RunIISummer16MiniAODv2/GluGluToHToTauTau_M125_13TeV_amcatnloFXFX_pythia8_tauola/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/',
    'numJobs' : 10,
    'apply_evtWeight' : True
   },
   'dy_lo_pythia8' : {
-   'inputFilePath' : '/store/mc/RunIISummer20UL18MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/270000/',
+   'inputFilePath' : '/cms/store/mc/RunIISummer20UL18MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/270000/',
    'numJobs' : 10,
    'apply_evtWeight' : False
   },
   'dy_nlo_pythia8' : {
-   'inputFilePath' : '/store/mc/RunIISummer20UL18MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/230000/',
+   'inputFilePath' : '/cms/store/mc/RunIISummer20UL18MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/230000/',
    'numJobs' : 10,
    'apply_evtWeight' : False
   },
